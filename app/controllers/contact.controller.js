2eebd3dfce5ef2db46734d1ddcfcb5a37d250a49
Tran Y <yb2003878@student.ctu.edu.vn>
2023-10-04 19:17:52 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,17 +1,64 @@
-exports.create = (req, res) => {
-    res.send({ message: "create handler" });
+const ApiError = require("../api-error");
+const MongoDB = require("../utils/mongodb.util");
+const ContactService = require("../services/contact.service");
+
+
+//Retrieve all contacts of a user from the database
+exports.findAll = async (req, res, next) => {
+    let documents = [];
+
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const { name } = req.query;
+        if (name) {
+            documents = await contactService.findByName(name);
+        } else {
+            documents = await contactService.find({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while retrieving contacts")
+        );
+    }
+
+    return res.send(documents);
+};
+
+//Find a single contact with an id
+exports.findOne = async (req, res, next) => {
+    
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.findbyId(req.params.id);
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
+            return next(
+                new ApiError(
+                    500,
+                    "Error retrieving contact with id=${req.params.id}"
+                )
+            );
 }
 
-exports.findAll = (req, res) => {
-    res.send({ message: "findAll handler" });
-}
+//Update a contact by the id in the request
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).length = 0){
+        return next(new ApiError(400, "Data to update can not be empty"));
+    }
 
-exports.findOne = (req, res) => {
-    res.send({ message: "findOne handler" });
-}
-
-exports.update = (req, res) => {
-    res.send({ message: "update handler" });
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.update(req.params.id, req.body);
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
+        return res.send({ message: "Contact was update successfully"});
+    } catch (error) {
+        return next(
+            new ApiError(500, "Error updating contact with id=${req.params.id}"
+            )
+        );
+    }
 }
 
 exports.delete = (req, res) => {
@@ -24,4 +71,17 @@ exports.deleteAll = (req, res) => {
 
 exports.findAllFavorite = (req, res) => {
     res.send({ message: "findAllFavorite handler" });
+}
+
+//Create and Save a new Contact
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name can not be empty"));
+    }
+
+    
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    
 }
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-//start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-})
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        const P0RT = config.app.port;
+        app.listen(P0RT, () => {
+            console.log(`Server is running on port ${P0RT}`);
+        });    
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+startServer();
