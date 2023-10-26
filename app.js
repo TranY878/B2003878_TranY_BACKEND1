467b207a3ddacf01181e283f16a77f145fa29b94
Tran Y <yb2003878@student.ctu.edu.vn>
2023-10-26 14:11:42 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/contacts', contactsRouter);
-//error
+
 app.use((req, res, next) => next(new ApiError(404, 'Resource not found')));
 
-app.use((err, req, res) => res.status(err.statusCode || 500).json({
+app.use((err, req, res, next) => res.status(err.statusCode || 500).json({
   message: err.message || 'Internal Server Error',
 }));
 app.get('/', (req, res) => {
--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -26,19 +26,19 @@ exports.findAll = async (req, res, next) => {
 
 //Find a single contact with an id
 exports.findOne = async (req, res, next) => {
-    
-        const contactService = new ContactService(MongoDB.client);
-        const document = await contactService.findbyId(req.params.id);
-        if (!document) {
-            return next(new ApiError(404, "Contact not found"));
-        }
-            return next(
-                new ApiError(
-                    500,
-                    "Error retrieving contact with id=${req.params.id}"
-                )
-            );
-}
+    try {
+      const contactService = new ContactService(MongoDB.client);
+      const document = await contactService.findById(req.params.id);
+      if (!document) {
+        return next(new ApiError(404, 'Contact not found'));
+      }
+      return res.send(document);
+    } catch (error) {
+      return next(
+        new ApiError(500, 'An error occurred while retrieving contact'),
+      );
+    }
+};
 
 //Update a contact by the id in the request
 exports.update = async (req, res, next) => {
@@ -61,27 +61,65 @@ exports.update = async (req, res, next) => {
     }
 }
 
-exports.delete = (req, res) => {
-    res.send({ message: "delete handler" });
+exports.delete = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.delete(req.params.id, req.body);
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
+        return res.send({ message: "Contact was delete successfully"});
+    } catch (error) {
+        return next(
+            new ApiError(500, "Could not delete contact with id=${req.params.id}"
+            )
+        );
+    }
 }
 
-exports.deleteAll = (req, res) => {
-    res.send({ message: "deleteAll handler" });
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.deleteAll();
+        return res.send({
+            message: "${deleteCount} contacts were deleted successfully",
+        });
+    } catch (error) {
+        return next(
+            new ApiError(500, "Error updating contact with id=${req.params.id}"
+            )
+        );
+    }
 }
 
-exports.findAllFavorite = (req, res) => {
-    res.send({ message: "findAllFavorite handler" });
+exports.findAllFavorite = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.findFavorite;
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500,
+                "An error occurred while retrieving favorite contacts"
+            )
+        );
+    }
 }
 
 //Create and Save a new Contact
 exports.create = async (req, res, next) => {
     if (!req.body?.name) {
-        return next(new ApiError(400, "Name can not be empty"));
-    }
-
+        return next(new ApiError(400, 'Name cannot be empty'));
+      }
     
+      try {
         const contactService = new ContactService(MongoDB.client);
+    
         const document = await contactService.create(req.body);
         return res.send(document);
-    
+      } catch (error) {
+        return next(
+          new ApiError(500, 'An error occurred while creating the contact'),
+        );
+      }
 }
